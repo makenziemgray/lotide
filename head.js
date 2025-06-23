a16a5cddf7201da26a50f8a1cd7b103e2dd05cf8
makenziemgray <makenziemgray0102@gmail.com>
2025-06-23 18:39:10 -0400

    Add array

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(array) {
+  return array[0];
+};
+
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([98]), 98);
+assertEqual(head([]), undefined);
