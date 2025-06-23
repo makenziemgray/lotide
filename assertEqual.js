85ee5d240a2e3023041c1134f3d228d4f16d9c5e
makenziemgray <makenziemgray0102@gmail.com>
2025-06-23 18:22:09 -0400

    add template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-if (actual === expected) {
-  console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-} else {
-  console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-}
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
