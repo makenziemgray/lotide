27e15b9fdc7c21e845bf2815802056cfd95d6fca
makenziemgray <makenziemgray0102@gmail.com>
2025-06-23 18:01:14 -0400

    Add assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function (actual, expected) {
+  🛑 Assertion Failed: Lighthouse Labs !== Bootcamp
+  ✅ Assertion Passed: 1 === 1
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
