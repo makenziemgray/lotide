e270ca84b1398f1754dba0bbce83c22e93875c75
makenziemgray <makenziemgray0102@gmail.com>
2025-06-26 17:18:31 -0400

    Implement takeUntil function and tests

---
+++ takeUntil.js
@@ -0,0 +1,30 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    if (callback(item)) {
+      break; // stop when callback returns a truthy value
+    }
+    results.push(item);
+  }
+
+  return results;
+};
+
