a1ef2e217b6626fbd5aaa71a1fb6d6ad27682d8b
makenziemgray <makenziemgray0102@gmail.com>
2025-06-30 09:09:47 -0400

    export function using module.export

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  
+const assertEqual = require("./assertEqual");
+if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
+
