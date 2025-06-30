df8289513d202ca245f92f295e2da3c0a154a966
makenziemgray <makenziemgray0102@gmail.com>
2025-06-30 09:10:29 -0400

    export function using module.export

--- head.js
+++ head.js
@@ -1,20 +1,17 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = require("./assertEqual");
+
+
+/* const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-};
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+}; */
+
 
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([98]), 98);
-assertEqual(head([]), undefined);
+module.exports = head;
