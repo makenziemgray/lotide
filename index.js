102d62ac393154ac3c21886997f8b2b0e29bf6d1
makenziemgray <makenziemgray0102@gmail.com>
2025-07-01 09:50:07 -0400

    Add central index.js to export all utility functions

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  countOnly,
+  countLetters,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
